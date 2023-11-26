c63ba19e01f8e6eb17766af38be88ee18301e933
junaid-repo <junaidraza3001@gmail.com>
2023-11-26 13:06:47 +0530

    next push

--- category/src/main/java/com/expense/tracker/category/repository/CategorySaveRepository.java
+++ category/src/main/java/com/expense/tracker/category/repository/CategorySaveRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.expense.tracker.category.entity.CategoryEntity;
 
-public interface CategorySaveRepository extends JpaRepository<CategoryEntity, Integer >{
+public interface CategorySaveRepository extends JpaRepository<CategoryEntity, String >{
 
 }
