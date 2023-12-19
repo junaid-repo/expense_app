ac263547059197daef0149d9fe913477dbf71c39
junaid-repo <junaidraza3001@gmail.com>
2023-12-19 15:22:41 +0530

    image to text

--- slogs/src/main/java/com/splitwise/app/slogs/TextFromImage.java
+++ slogs/src/main/java/com/splitwise/app/slogs/TextFromImage.java
@@ -2,26 +2,29 @@ package com.splitwise.app.slogs;
 
 import java.io.File;
 
-import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.ITessAPI.TessBaseAPI;
 
 public class TextFromImage {
-	public String getImgText(String imageLocation) {
-		ITesseract iT = new Tesseract();
-		String imgText = "";
-		try {
-			imgText = iT.doOCR(new File(imageLocation));
-			return imgText;
-		} catch (TesseractException e) {
-			e.printStackTrace();
-			// return “Error while reading image”;
-		}
-		return imgText;
-	}
 
 	public static void main(String[] args) {
-		TextFromImage app = new TextFromImage();
-		System.out.println(app.getImgText("/home/junaid/Pictures/Screenshot_20231219_122624.png"));
+
+		String imageLocation = "/home/junaid/Pictures/IMG20231219151512.jpg";
+		File image = new File(imageLocation);
+		Tesseract tesseract = new Tesseract();
+
+		tesseract.setDatapath("/home/junaid/Workspaces/expense_app/slogs/src/main/resources/tessdata");
+
+		String result = "";
+		try {
+			result = tesseract.doOCR(image);
+		} catch (TesseractException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(result);
+
 	}
+
 }
