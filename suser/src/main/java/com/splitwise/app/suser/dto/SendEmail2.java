16ce9a2a17eb9d946f78314ffb5c62685dc2705a
junaid-repo <junaidraza3001@gmail.com>
2023-12-19 16:23:12 +0530

    email changes

--- suser/src/main/java/com/splitwise/app/suser/dto/SendEmail2.java
+++ suser/src/main/java/com/splitwise/app/suser/dto/SendEmail2.java
@@ -1,36 +1,33 @@
-package com.splitwise.app.slogs;
-
-import com.mailjet.client.errors.MailjetException;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
+package com.splitwise.app.suser.dto;
 
 import com.mailjet.client.MailjetClient;
 import com.mailjet.client.MailjetRequest;
 import com.mailjet.client.MailjetResponse;
+import com.mailjet.client.errors.MailjetException;
 import com.mailjet.client.resource.Email;
 
 public class SendEmail2 {
 	/**
 	 * This call sends an email to one recipient.
 	 */
-	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws MailjetException {
 		MailjetClient client;
 		MailjetRequest request;
 		MailjetResponse response;
-		client = new MailjetClient("318c84dd71aef348d900be5cacf99be9", "2d06252cc41a476fe0c7a0bbc6ca601d");
-		request = new MailjetRequest(Email.resource).property(Email.FROMEMAIL, "shop@ranchinovelstore.com")
+		client = new MailjetClient(System.getenv("MJ_APIKEY_PUBLIC"), System.getenv("MJ_APIKEY_PRIVATE"));
+		request = new MailjetRequest(Email.resource).property(Email.FROMEMAIL, "pilot@mailjet.com")
 				.property(Email.FROMNAME, "Mailjet Pilot").property(Email.SUBJECT, "Your email flight plan!")
 				.property(Email.TEXTPART, "Dear passenger, welcome to Mailjet! May the delivery force be with you!")
 				.property(Email.HTMLPART,
-						"<h3>Dear passenger, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!<br />May the delivery force be with you!")
-
-				.property(Email.RECIPIENTS,
-						new JSONArray().put(new JSONObject().put("Email", "junaidrana926@gmail.com")));
-
+						"<h3>Dear passenger, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!<br />May the delivery force be with you!");
+		/*
+		 * .property(Email.RECIPIENTS, new JSONArray().put(new JSONObject().put("Email",
+		 * "passenger@mailjet.com")));
+		 */
 		response = client.post(request);
 		System.out.println(response.getStatus());
 		System.out.println(response.getData());
 	}
+
+
 }
